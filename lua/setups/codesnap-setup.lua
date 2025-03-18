017d1dbc51e89a26f7cff12c47cecfa5a51a5978
Kenedy Nopriansyah <kenedynopriansyah@192.168.1.11>
2025-03-18 20:00:26 +0700

    update

--- lua/setups/codesnap-setup.lua
+++ lua/setups/codesnap-setup.lua
@@ -1,7 +1,7 @@
 local codesnap = require('codesnap')
 
 codesnap.setup({
-  bg_theme = "dusk",
+  bg_theme = "grape",
   title = "Kenedy Nopriansyah",
   watermark = "Kenedy Nopriansyah",
   save_path = "~/Desktop"
